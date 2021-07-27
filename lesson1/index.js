2b5311629ab20f0209e00802de5c4e7270cef125
fdyb <fdyb@yandex.ru>
2021-07-27 14:58:32 +0300

    lesson 1

---
+++ lesson1/index.js
@@ -0,0 +1,35 @@
+const colors_module = require("colors/safe")
+
+const colors = [colors_module.red, colors_module.yellow, colors_module.green]
+
+function isNumber(val) {
+    return /^\d+$/.test(val)
+}
+
+function simpleNumbers(n) {
+    let inputArr = []
+    for (let i = 1; i <= n; i++) {
+        inputArr.push(i)
+    }
+    let p = 2
+    while (p < n) {
+        inputArr = inputArr.filter(item => !(((item % p) == 0) && (item > p)))
+        p = inputArr.find(item => (item>p))
+    }
+    return inputArr
+}
+
+const N = process.argv[2]
+
+if (!isNumber(N)) {
+    console.log('enter number value')
+}
+else {
+    let result = simpleNumbers(parseInt(N))
+    let x = 0
+    result.forEach(item => {
+        console.log(colors[x](item));
+        x = (x+1) == 3? 0 : x+1;
+    })
+}
+
